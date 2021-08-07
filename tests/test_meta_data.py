99a1bc0e7d871433850966edaf3e96f94c70084c
Toyin Hawau Olamide <hawau.olamide.th@gmail.com>
2021-08-07 20:17:56 +0100

    tests

---
+++ tests/test_meta_data.py
@@ -0,0 +1,30 @@
+import unittest
+import pandas as pd
+import sys, os 
+sys.path.append(os.path.abspath(os.path.join('../scripts')))
+from MetaDataGeneration import Create_meta_data
+
+sys.path.append(os.path.abspath(os.path.join('../data/test')))
+
+trans_path = 'test.txt'
+audio_path = '/wav5'
+
+meta = Create_meta_data(transcription_path=trans_path, audio_path=audio_path,
+                        audio_extension='.wav')
+
+class TestMeta_Data(unittest.TestCase):
+    """
+    A class for unit-testing functiosns in the MetaDataGeneration.py file.
+    """
+    def setUp(self):
+        self.audio_extension = '.wav'
+        return self.audio_extension
+         
+    def test_get_filenameAndTranscription(self):
+        self.assertIsInstance( meta.get_filenameAndTranscription(), dict)
+
+
+
+if __name__ == '__main__':
+	unittest.main()
+
