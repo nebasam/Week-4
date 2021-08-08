86d785a71305428210bd55e925c88fd75cc7f91b
nebasam <neba.samuel17@gmail.com>
2021-08-08 21:45:17 +0000

    creates class audio_generator

--- scripts/data_generator.py
+++ scripts/data_generator.py
@@ -27,3 +27,208 @@ def make_audio_gen(train_json,
     return AudioGenerator(train_json, valid_json, minibatch_size=minibatch_size, 
         spectrogram=spectrogram, mfcc_dim=mfcc_dim, max_duration=max_duration,
         sort_by_duration=sort_by_duration)
+class AudioGenerator():
+    def __init__(self, train_corpus, valid_corpus, step=10, window=20, max_freq=8000, mfcc_dim=13,
+        minibatch_size=20, desc_file=None, spectrogram=True, max_duration=10.0, 
+        sort_by_duration=False):
+        """
+        Params:
+            step (int): Step size in milliseconds between windows (for spectrogram ONLY)
+            window (int): FFT window size in milliseconds (for spectrogram ONLY)
+            max_freq (int): Only FFT bins corresponding to frequencies between
+                [0, max_freq] are returned (for spectrogram ONLY)
+            desc_file (str, optional): Path to a JSON-line file that contains
+                labels and paths to the audio files. If this is None, then
+                load metadata right away
+        """
+        self.train_corpus = train_corpus
+        self.valid_corpus = valid_corpus
+        self.feat_dim = calc_feat_dim(window, max_freq)
+        self.mfcc_dim = mfcc_dim
+        self.feats_mean = np.zeros((self.feat_dim,))
+        self.feats_std = np.ones((self.feat_dim,))
+        self.rng = random.Random(RNG_SEED)
+        if desc_file is not None:
+            self.load_metadata_from_desc_file(desc_file)
+        self.step = step
+        self.window = window
+        self.max_freq = max_freq
+        self.cur_train_index = 0
+        self.cur_valid_index = 0
+        self.cur_test_index = 0
+        self.max_duration=max_duration
+        self.minibatch_size = minibatch_size
+        self.spectrogram = spectrogram
+        self.sort_by_duration = sort_by_duration
+
+    def get_batch(self, partition):
+        """ Obtain a batch of train, validation, or test data
+        """
+        if partition == 'train':
+            audio_paths = self.train_audio_paths
+            cur_index = self.cur_train_index
+            texts = self.train_texts
+        elif partition == 'valid':
+            audio_paths = self.valid_audio_paths
+            cur_index = self.cur_valid_index
+            texts = self.valid_texts
+        elif partition == 'test':
+            audio_paths = self.test_audio_paths
+            cur_index = self.test_valid_index
+            texts = self.test_texts
+        else:
+            raise Exception("Invalid partition. "
+                "Must be train/validation")
+
+        features = [self.normalize(self.featurize(a)) for a in 
+            audio_paths[cur_index:cur_index+self.minibatch_size]]
+
+        # calculate necessary sizes
+        max_length = max([features[i].shape[0] 
+            for i in range(0, self.minibatch_size)])
+        max_string_length = max([len(texts[cur_index+i]) 
+            for i in range(0, self.minibatch_size)])
+        
+        # initialize the arrays
+        X_data = np.zeros([self.minibatch_size, max_length, 
+            self.feat_dim*self.spectrogram + self.mfcc_dim*(not self.spectrogram)])
+        labels = np.ones([self.minibatch_size, max_string_length]) * 28
+        input_length = np.zeros([self.minibatch_size, 1])
+        label_length = np.zeros([self.minibatch_size, 1])
+        
+        for i in range(0, self.minibatch_size):
+            # calculate X_data & input_length
+            feat = features[i]
+            input_length[i] = feat.shape[0]
+            X_data[i, :feat.shape[0], :] = feat
+
+            # calculate labels & label_length
+            label = np.array(text_to_int_sequence(texts[cur_index+i])) 
+            labels[i, :len(label)] = label
+            label_length[i] = len(label)
+ 
+        # return the arrays
+        outputs = {'ctc': np.zeros([self.minibatch_size])}
+        inputs = {'the_input': X_data, 
+                  'the_labels': labels, 
+                  'input_length': input_length, 
+                  'label_length': label_length 
+                 }
+        return (inputs, outputs)
+
+    def shuffle_data_by_partition(self, partition):
+        """ Shuffle the training or validation data
+        """
+        if partition == 'train':
+            self.train_audio_paths, self.train_durations, self.train_texts = shuffle_data(
+                self.train_audio_paths, self.train_durations, self.train_texts)
+            self.train_length = len(self.train_texts)
+        elif partition == 'valid':
+            self.valid_audio_paths, self.valid_durations, self.valid_texts = shuffle_data(
+                self.valid_audio_paths, self.valid_durations, self.valid_texts)
+            self.valid_length = len(self.valid_texts)
+        else:
+            raise Exception("Invalid partition. "
+                "Must be train/validation")
+
+    def sort_data_by_duration(self, partition):
+        """ Sort the training or validation sets by (increasing) duration
+        """
+        if partition == 'train':
+            self.train_audio_paths, self.train_durations, self.train_texts = sort_data(
+                self.train_audio_paths, self.train_durations, self.train_texts)
+        elif partition == 'valid':
+            self.valid_audio_paths, self.valid_durations, self.valid_texts = sort_data(
+                self.valid_audio_paths, self.valid_durations, self.valid_texts)
+        else:
+            raise Exception("Invalid partition. "
+                "Must be train/validation")
+
+    def next_train(self):
+        """ Obtain a batch of training data
+        """
+        while True:
+            ret = self.get_batch('train')
+            self.cur_train_index += self.minibatch_size
+            if self.cur_train_index >= len(self.train_texts) - self.minibatch_size:
+                self.cur_train_index = 0
+                self.shuffle_data_by_partition('train')
+            yield ret    
+
+    def next_valid(self):
+        """ Obtain a batch of validation data
+        """
+        while True:
+            ret = self.get_batch('valid')
+            self.cur_valid_index += self.minibatch_size
+            if self.cur_valid_index >= len(self.valid_texts) - self.minibatch_size:
+                self.cur_valid_index = 0
+                self.shuffle_data_by_partition('valid')
+            yield ret
+
+    def next_test(self):
+        """ Obtain a batch of test data
+        """
+        while True:
+            ret = self.get_batch('test')
+            self.cur_test_index += self.minibatch_size
+            if self.cur_test_index >= len(self.test_texts) - self.minibatch_size:
+                self.cur_test_index = 0
+            yield ret
+
+    def load_train_data(self):
+        desc_file=self.train_corpus
+        self.load_metadata_from_desc_file(desc_file, 'train')
+        self.fit_train()
+        if self.sort_by_duration:
+            self.sort_data_by_duration('train')
+
+    def load_validation_data(self):
+        desc_file=self.valid_corpus
+        self.load_metadata_from_desc_file(desc_file, 'validation')
+        if self.sort_by_duration:
+            self.sort_data_by_duration('valid')
+
+    def load_test_data(self):
+        desc_file='test_corpus.json'
+        self.load_metadata_from_desc_file(desc_file, 'test')
+    
+    def load_metadata_from_desc_file(self, desc_file, partition):
+        """ Read metadata from a JSON-line file
+            (possibly takes long, depending on the filesize)
+        Params:
+            desc_file (str):  Path to a JSON-line file that contains labels and
+                paths to the audio files
+            partition (str): One of 'train', 'validation' or 'test'
+        """
+        audio_paths, durations, texts = [], [], []
+        with open(desc_file, encoding='utf-8') as json_line_file:
+            for line_num, json_line in enumerate(json_line_file):
+                try:
+                    spec = json.loads(json_line)
+                    if float(spec['duration']) > self.max_duration:
+                        continue
+                    audio_paths.append(spec['key'])
+                    durations.append(float(spec['duration']))
+                    texts.append(spec['text'])
+                except Exception as e:
+                    # Change to (KeyError, ValueError) or
+                    # (KeyError,json.decoder.JSONDecodeError), depending on
+                    # json module version
+                    print('Error reading line #{}: {}'
+                                .format(line_num, json_line))
+        if partition == 'train':
+            self.train_audio_paths = audio_paths
+            self.train_durations = durations
+            self.train_texts = texts
+        elif partition == 'validation':
+            self.valid_audio_paths = audio_paths
+            self.valid_durations = durations
+            self.valid_texts = texts
+        elif partition == 'test':
+            self.test_audio_paths = audio_paths
+            self.test_durations = durations
+            self.test_texts = texts
+        else:
+            raise Exception("Invalid partition to load metadata. "
+             "Must be train/validation/test")
