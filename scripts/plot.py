615b06638c04641b50bf33e05d9b30b61d38536d
nebasam <neba.samuel17@gmail.com>
2021-08-09 07:59:16 +0000

    modifies model

--- scripts/plot.py
+++ scripts/plot.py
@@ -6,10 +6,11 @@ import pickle
 import matplotlib.pyplot as plt
 
 def plot_hist(p):
-    hist = pickle.load(open( "models/" + p + ".pickle", "rb"))
+    hist = pickle.load(open( "../models/" + p + ".pickle", "rb"))
     plt.plot(hist['loss'], label="train")
     plt.plot(hist['val_loss'], label="valid")
     plt.xlabel("epoch")
     plt.ylabel("loss")
     plt.legend()
     plt.show
+    print ('i am df here')
