ab0dc2293bd64c4c793c3f207c174a1c47d04877
nebasam <neba.samuel17@gmail.com>
2021-08-10 13:51:47 +0000

    creates ccalculation of wer scripts

---
+++ scripts/wer.py
@@ -0,0 +1,26 @@
+# Code adapted from https://github.com/tilayealemu/MelaNet/blob/master/model_2.ipynbg
+def wer(r, h):
+    """
+    Calculation of WER with Levenshtein distance.
+
+    Works only for iterables up to 254 elements (uint8).
+    O(nm) time ans space complexity.
+
+    Parameters
+    ----------
+    r : list
+    h : list
+
+    Returns
+    -------
+    int
+
+    Examples
+    --------
+    >>> wer("who is there".split(), "is there".split())
+    1
+    >>> wer("who is there".split(), "".split())
+    3
+    >>> wer("".split(), "who is there".split())
+    3
+    """
