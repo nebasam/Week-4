ff28bcaf1b29ea8d733109680a119ef0d5af1537
nebasam <neba.samuel17@gmail.com>
2021-08-10 13:54:16 +0000

    creates computation for wer

--- scripts/wer.py
+++ scripts/wer.py
@@ -34,3 +34,15 @@ def wer(r, h):
                 d[0][j] = j
             elif j == 0:
                 d[i][0] = i
+    # computation
+    for i in range(1, len(r)+1):
+        for j in range(1, len(h)+1):
+            if r[i-1] == h[j-1]:
+                d[i][j] = d[i-1][j-1]
+            else:
+                substitution = d[i-1][j-1] + 1
+                insertion    = d[i][j-1] + 1
+                deletion     = d[i-1][j] + 1
+                d[i][j] = min(substitution, insertion, deletion)
+
+    return d[len(r)][len(h)]
