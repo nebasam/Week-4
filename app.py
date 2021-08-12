0a45a5bb2e4fa185fa94ed9d85342325505b99da
Nebiyu Samuel <neba.samuel17@gmail.com>
2021-08-12 13:01:52 +0300

    enables upload audio file

--- app.py
+++ app.py
@@ -3,10 +3,15 @@ import os
 import pandas as pd
 import pickle
 import sys
+import os
+import shutil
 import numpy as np
 from werkzeug.utils import secure_filename, send_file
 
 app = Flask(__name__)
+
+
+MODEL_PATH = 'model/deep.h5'
 
 @app.route("/assets/<path:path>")
 def static_dir(path):
@@ -18,7 +23,40 @@ def index():
     if request.method == 'GET':
         return render_template("index.html")
 
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    global class_names
+    if request.method == 'POST':
+        print('called')
+        # Get the file from post request
+        print('Request files', request.files['audio'])
+        f = request.files['audio']
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads/audio', secure_filename(f.filename))
+        f.save(file_path)
+        # print(f)
 
+        #Make Prediction
+        preds = model_predict(file_path, model)
+        shutil.rmtree('./uploads/audio')
+        os.mkdir('./uploads/audio')
+
+
+        return '<h1>Sup World!</h1>'
+
+
+
+        # df = pd.read_csv(file_path, parse_dates=True, index_col="Date")
+        # # print(df.head())
+        # df['Year'] = df.index.year
+        # df['Month'] = df.index.month
+        # df['Day'] = df.index.day
+        # df['WeekOfYear'] = df.index.weekofyear
+        # results = make_prediction(df)
+        # print('Printing result',results[0])
+        # return str(int(results[0]))
 
 @app.route("/about")
 def about():
